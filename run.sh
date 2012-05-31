d191373a79466ff1e31114dfa191c56c85783429
Vlad Niculae <vlad@vene.ro>
2012-05-31 10:48:21 +0300

    Added run script

---
+++ run.sh
@@ -0,0 +1,1 @@
+scala -cp target/scala-2.9.2/old-romanian-ir-example_2.9.2-0.1.jar:target/scala-2.9.2/lucene-core-3.6.0.jar:target/scala-2.9.2/lucene-analyzers-3.6.0.jar RetrieveOldRomanian
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,6 +3,14 @@ import org.apache.lucene.util.Version
 object RetrieveOldRomanian extends Application {
         val analyzer = new RomanianAnalyzer(Version.LUCENE_CURRENT)
         val directory = IndexBuilder.buildIndexDirectory(analyzer)
-
-        new Searcher(analyzer, directory).search("Unguresc", false)
+        var loop = true
+        while (loop)
+        {
+        	printf("Interogare> ")
+            var query = readLine()
+            if (query != "")
+                new Searcher(analyzer, directory).search(query, false)
+            else
+                loop = false
+        }
 }
